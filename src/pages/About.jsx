47d98cd25a010654cbc0560eede9eb583a1e95d0
Sallai András <andrewteki@gmail.com>
2026-02-10 23:50:05 +0100

    Az About.jsx javítva

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,9 @@ const About = () => {
             <h1>Névjegy</h1>
             <p>Sallai András</p>
             <p>2026-02-10</p>
+            <p>
+                <a href="https://szhit.hu/m/telemez">https://szhit.hu/m/telemez</a>
+            </p>
         </div>
     )
 }
