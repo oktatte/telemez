c9b4559b1ab0b3fe6e3cfa460a883707d45cfcea
Sallai András <andrewteki@gmail.com>
2026-02-10 23:51:18 +0100

    Az About.jsx javítva

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,9 @@ const About = () => {
     return (
         <div>
             <h1>Névjegy</h1>
-            <p>Sallai András</p>
+            <p>Copyright (C) 2026, Sallai András</p>
             <p>2026-02-10</p>
+            <p>Licenc: MIT</p>
             <p>
                 <a href="https://szhit.hu/m/telemez">https://szhit.hu/m/telemez</a>
             </p>
